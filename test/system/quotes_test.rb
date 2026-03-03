f7e38cdd86e42de288aa9f4825cd855594d6b63b
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-03-03 16:28:47 +0500

    change system tests to adapt to turbo frames

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -11,18 +11,15 @@ class QuotesTest < ApplicationSystemTestCase
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
     click_on "New quote"
-    assert_selector "h1", text: "New quote"
+    fill_in "Name", with: "Capybara quote"
 
     # When we fill in the name input with "Capybara quote"
     # and we click on "Create Quote"
-    fill_in "Name", with: "Capybara quote"
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
+    # We would see our "Capybara quote" added to the list
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
   end
@@ -39,9 +36,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
