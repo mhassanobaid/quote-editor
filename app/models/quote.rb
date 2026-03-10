69855aa112c97b7d4290b97f38e28583f5450ca6
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-03-10 12:17:45 +0500

    Applied Turbo Streams conventions and syntactic sugar

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,8 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # By default, the target option will be equal to model_name.plural, which is equal to "quotes" in the context of our Quote model. Thanks to this convention, we can remove the target: "quotes" option:
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+
+  after_create_commit -> { broadcast_prepend_to "quotes" }
 end
