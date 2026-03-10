dc478c5dc9b702a85c50a70df085ec7da2b51781
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-03-10 12:46:43 +0500

    Made broadcasting asynchronous with ActiveJob

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,7 +5,15 @@ class Quote < ApplicationRecord
   # By default, the target option will be equal to model_name.plural, which is equal to "quotes" in the context of our Quote model. Thanks to this convention, we can remove the target: "quotes" option:
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
 
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
+=begin
+# synchronous so performance will be low
+after_create_commit -> { broadcast_prepend_to "quotes" }
+after_update_commit -> { broadcast_replace_to "quotes" }
+after_destroy_commit -> { broadcast_remove_to "quotes" }
+=end
+
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # destroy method of broadcast_remove_later_to not exists bcz it is not possible to perform job after quote is deleted
   after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
