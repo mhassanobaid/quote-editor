5f55592cafa339483443a0c2e1489f7cc4043386
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-03-26 13:11:41 +0500

    Deleted line items without Turbo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
-  before_action :set_line_item, only: [:edit, :update]
+  before_action :set_line_item, only: [:edit, :update, :destroy]
 
   def new
     @line_item = @line_item_date.line_items.build
@@ -26,6 +26,12 @@ class LineItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @line_item.destroy
+
+    redirect_to quote_path(@quote), notice: "Item was successfully destroyed."
   end
 
   private
