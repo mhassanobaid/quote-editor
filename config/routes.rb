4a09222a4f35edc9a43b17b2af7cb358a8d21b8c
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-04-01 15:36:01 +0500

    added health route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/up", to: proc { [200, {}, ["OK"]] }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: "pages#home"
