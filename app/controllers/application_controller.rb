83c6f63f8e0c1c67405052f0821be2417875da75
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-04-03 06:47:35 +0500

    restrictions on ui for non admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :admin_namespace?
 
   def admin_namespace?
-      params[:controller].start_with?("admin/") ||
-      params[:controller].start_with?("active_admin/")
+    params[:controller].start_with?("admin/") ||
+    params[:controller].start_with?("active_admin/")
   end
 
   def current_company
