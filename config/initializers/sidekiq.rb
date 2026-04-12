0d131d7aeceb09ba2434aac1ab46d6e4ced19574
mhassanobaid <hassanobaidkhan999160@gmail.com>
2026-04-12 23:33:16 +0500

    Finalize production Docker setup + CI/CD

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
 require 'sidekiq'
 
+redis_url = ENV["REDIS_URL"]
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }}
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }}
+  config.redis = { url: redis_url }
 end
